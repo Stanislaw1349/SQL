aa99002ce420c39dc7ea01c48dc7f664ec04d880
Stanislaw1349 <alfstas@gmail.com>
2023-09-23 21:54:34 +0300

    add queries from mywork.dept

--- Query 2.sql
+++ Query 2.sql
@@ -78,24 +78,36 @@ select * from mywork.dept where deptno between 1 and 3;
 select * from mywork.dept where deptno in (1,2,3);
 select * from mywork.dept where deptno >=1 and deptno <4;
 
-/*  
-Part 1 - mywork database
-Write sql 
-	1. Add column 'country' to dept table in mywork database
-	2. Rename column 'loc' to 'city'
-	3. Add three more departments: HR, Engineering, Marketing
-	4. Write sql statement to show which department is in Atlanta
+
+-- Part 1 - mywork database
+
+	-- 1. Add column 'country' to dept table in mywork database
+    alter table mywork.dept
+    add column country varchar (50);
+    
+	-- 2. Rename column 'loc' to 'city'
+    alter table mywork.dept rename column loc to city;
+    
+	-- 3. Add three more departments: HR, Engineering, Marketing
+    insert into mywork.dept (deptno, dname)
+    values
+    (5, 'HR'),
+    (6, 'Engineering'),
+    (7, 'Marketing');
+    
+	-- 4. Write sql statement to show which department is in Atlanta
+    select deptno, dname, city from mywork.dept 
+    where city = 'Atlanta';
     
 
 
-Part 2  - library_simple database
-Run library_simple.sql script  (takes a few minutes)
-(source: https://github.com/amyasnov/stepic-db-intro/tree/2650f9a7f9c72e1219ea93cb4c4e410cca046e54/test)
+-- Part 2  - library_simple database
+-- Run library_simple.sql script  (takes a few minutes)
+-- (source: https://github.com/amyasnov/stepic-db-intro/tree/2650f9a7f9c72e1219ea93cb4c4e410cca046e54/test)
 
-Look at table relationships in EER Diagram
+-- Look at table relationships in EER Diagram
 
-	1. What is the first name of the author with the last name Swanson?
-	2. How many pages are in Men Without Fear book?
-	3. Show all book categories that start with with letter 'W'
-*/
+	-- 1. What is the first name of the author with the last name Swanson?
+	-- 2. How many pages are in Men Without Fear book?
+	-- 3. Show all book categories that start with with letter 'W'
  
